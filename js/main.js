ff4972df99997f1e72e07ea1a4ea236a8e3b9a15
kevinfan23 <kevinfan23@ucla.edu>
2016-05-27 10:54:33 -0700

    add chart

--- js/main.js
+++ js/main.js
@@ -95,14 +95,37 @@ function isElementInViewport(elem) {
 // Check if it's time to start the radial progress animation.
 function drawFillAnimation() {
 	
-    var $elem = $('.draw-fill-container');
+    var $elem1 = $('.draw-fill-container');
+    var $elem2 = $('#chart');
+
 
     // If the animation has already been started
     //if ($elem1.hasClass('fill-one')) return;
     //if ($elem2.hasClass('fill-two')) return;
-    if (isElementInViewport($elem)) {
+    if (isElementInViewport($elem1)) {
         // Start the animation
         $('.draw-fill-container g').attr('class', 'drawing');
        // $('.draw-fill-container g').attr('class', 'filling');
     }
+    if (isElementInViewport($elem2)) {
+        // Start the animation
+        updateNumber();
+        $('#chart g').attr('class', 'drawing');
+    }
+}
+
+function updateNumber() {
+	var index = 0;
+	var clr = null;
+	(loop = function() {
+    clearTimeout(clr);
+    (inloop = function() {
+		if (index == 24) {
+      		return;
+     	}
+      $('#chart-index').html("106.11 (" + index + "%)");
+      index += 1
+      clr = setTimeout(inloop, 60);
+    })();
+    })();
 }
